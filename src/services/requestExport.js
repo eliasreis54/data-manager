1f01e1a0453c372af03a9511dbe205fbbc3ed02d
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-01-14 11:33:17 +0000

    Deploy eliasreis54/data-manager to github.com/eliasreis54/data-manager.git:gh-pages

--- src/services/requestExport.js
+++ src/services/requestExport.js
@@ -17,18 +17,35 @@ function clearTemplateRet(template) {
   return template;
 }
 
-function clearDeviceRet(devices) {
+function clearDeviceRet(devices, templates) {
   devices.forEach((device) => {
     const item = device;
+    delete item.created;
+    delete item.updated;
     const keys = Object.keys(item.attrs);
     const newAttrs = [];
     keys.forEach((key) => {
       item.attrs[key].forEach((obj) => {
         const aux = obj;
-        delete aux.id;
-        delete aux.created;
-        delete aux.metadata;
-        newAttrs.push(aux);
+        templates.forEach((template) => {
+          template.attrs.forEach((templateAttr) => {
+            if (templateAttr.id === aux.id) {
+              const templateKeys = Object.keys(templateAttr);
+              delete aux.created;
+              let allEqual = true;
+              templateKeys.forEach((keyValue) => {
+                if ((aux[keyValue] === templateAttr[keyValue]) && (allEqual === true)) {
+                  allEqual = true;
+                } else {
+                  allEqual = false;
+                }
+              });
+              if (!allEqual) {
+                newAttrs.push(aux);
+              }
+            }
+          });
+        });
       });
     });
     delete item.attrs;
@@ -37,19 +54,20 @@ function clearDeviceRet(devices) {
   return devices;
 }
 
-const requestExport = () => new Promise((resolve, reject) => {
+const requestExport = token => new Promise((resolve, reject) => {
   logger.debug('Will export data');
   const requests = [
-    requestDevice.get(),
-    requestTemplate.get(),
-    requestFlow.get(),
+    requestDevice.get(token),
+    requestTemplate.get(token),
+    requestFlow.get(token),
   ];
   Promise.all(requests)
     .then((ret) => {
       logger.debug('Data received.');
+      const templatesRet = clearTemplateRet(ret[1].templates);
       const allData = {
-        devices: clearDeviceRet(ret[0].devices),
-        templates: clearTemplateRet(ret[1].templates),
+        devices: clearDeviceRet(ret[0].devices, templatesRet),
+        templates: templatesRet,
         flows: ret[2].flows,
       };
       resolve(allData);
